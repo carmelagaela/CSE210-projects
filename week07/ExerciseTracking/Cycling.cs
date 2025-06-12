d5614f9e982a0c65ef9bdb9669e1861930f9e219
Carmela Gaela <mariacarmela.gaela@gmail.com>
2025-06-12 20:17:45 +0800

    Done working on Week 07 Excercise Tracking

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,14 @@
+public class Cycling : Activity
+{
+    private double _speed; // in mph
+
+    public Cycling(DateTime date, int lengthInMinutes, double speed)
+        : base(date, lengthInMinutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetSpeed() => _speed;
+    public override double GetDistance() => (_speed * GetLengthInMinutes()) / 60;
+    public override double GetPace() => 60 / _speed;
+}
---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,14 @@
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(DateTime date, int lengthInMinutes, int laps)
+        : base(date, lengthInMinutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance() => _laps * 50 / 1000.0 * 0.62;
+    public override double GetSpeed() => (GetDistance() / GetLengthInMinutes()) * 60;
+    public override double GetPace() => GetLengthInMinutes() / GetDistance();
+}
